abfabc35fa516b7ac34c4f626a592dc3d0e0bf9a
Benjamin Marconnet <benjamin@marconnet.fr>
2016-07-09 16:47:18 +0200

    Improve testing for searc algorithms

--- EssentialAlgorithms/Searching/_Testing.cs
+++ EssentialAlgorithms/Searching/_Testing.cs
@@ -1,30 +1,23 @@
-﻿using System;
-
-namespace EssentialAlgorithms.Searching
+﻿namespace EssentialAlgorithms.Searching
 {
     class _Testing
     {
         public static void Test()
         {
             int[] array = { 7, 4, 5, 1, 6, 3, 2, 8 };
-            int[] sortedArray = new int[array.Length];
-            Array.Copy(array, sortedArray, array.Length);
-            Sorting.Heapsort.Sort(sortedArray);
+            Sorting.Heapsort.Sort(array);
 
             int res = -1;
             int target = 5;
 
-            res = LinearSearch.Searching(sortedArray, target);
-            System.Diagnostics.Debug.WriteLine("Linear search: idx of {0} is {1}",
-                target, res);
+            res = LinearSearch.Searching(array, target);
+            System.Diagnostics.Debug.WriteLine("Linear search: idx of {0} is {1}", target, res);
 
-            res = BinarySearch.Searching(sortedArray, target);
-            System.Diagnostics.Debug.WriteLine("Binary search: idx of {0} is {1}",
-                target, res);
+            res = BinarySearch.Searching(array, target);
+            System.Diagnostics.Debug.WriteLine("Binary search: idx of {0} is {1}", target, res);
 
-            res = InterpolationSearch.Searching(sortedArray, target);
-            System.Diagnostics.Debug.WriteLine("Interpolation search: idx of {0} is {1}",
-                target, res);
+            res = InterpolationSearch.Searching(array, target);
+            System.Diagnostics.Debug.WriteLine("Interpolation search: idx of {0} is {1}", target, res);
         }
     }
 }
